2943c83be4c45a87c0a167f14e4a1bf236b954e1
colemaih <colemaih@mail.uc.edu>
2023-09-14 15:24:51 -0400

    Part 1 Lab 04

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,43 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+       // intOperandA, intOperandB, intSum, intProduct, intDifference, intQuotient, intModulo
+       int intOperandA = 10;
+       int intOperandB = 5;
+
+       int intProduct= 0;
+       int intSum = 0;
+       int intDifference= 0;
+       int intQuotient= 0;
+       int intModulo= 0;
+
+
+       intSum = intOperandA + intOperandB;
+        System.out.println( "The sum of " + intOperandA + " and " + intOperandB + " is " + intSum);
+
+        intProduct= intOperandA * intOperandB;
+        System.out.println("The product of " + intOperandA + " and " + intOperandB + " is " + intProduct);
+
+        intDifference= intOperandA - intOperandB;
+        System.out.println( "The Difference of " + intOperandA + " and " + intOperandB + " is " + intDifference);
+
+        intQuotient = intOperandA / intOperandB;
+        System.out.println( "The Quotient of " + intOperandA + " and " + intOperandB + " is " + intQuotient + " with a remainder of " + intModulo);
+
+        // rest of the int statement go here for product, difference, quotient, modulo
+        double doubleOperandA = 62.5;
+        double doubleOperandB = 21.5;
+        double doubleSum= doubleOperandA + doubleOperandB;
+        double doubleDifference = doubleOperandA - doubleOperandB;
+        double doubleProduct = doubleOperandA * doubleOperandB;
+        double doubleQuotient = doubleOperandA / doubleOperandB;
+        System.out.println("The sum of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum );
+
+        System.out.println("The product of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleProduct );
+
+        System.out.println("The Difference of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleDifference );
+
+        System.out.println("The Quotient of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleQuotient );
+
     }
 }
